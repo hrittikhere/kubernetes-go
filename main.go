7fff00afda7642035276a5f5440013ea8e1ed1da
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2022-08-04 20:20:23 +0000

    Add client

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func main() {
+
+	client := getClient()
+	pods := client.CoreV1().Pods("default")
+
+
+}
+
+func getClient() *kubernetes.Clientset {
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", filepath.Join(homedir.HomeDir(), ".kube", "config"))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return clientset
+}
